0de0d810204538c6dde71777f4ee110d82e917b7
Emiliano Borghi <emiliano@smartmachine.nz>
2023-07-25 13:37:50 +1200

    Fix GPU extraction
    Signed-off-by: Emiliano Borghi <emiliano@smartmachine.nz>

--- gpu/segmentation/include/pcl/gpu/segmentation/impl/gpu_extract_clusters.hpp
+++ gpu/segmentation/include/pcl/gpu/segmentation/impl/gpu_extract_clusters.hpp
@@ -181,10 +181,10 @@ pcl::gpu::extractEuclideanClusters(
       r.header = host_cloud_->header;
       clusters.push_back(r); // We could avoid a copy by working directly in the vector
     }
-    else if (seed_queue.size () > max_pts_per_cluster)
+    else if (found_points.size () > max_pts_per_cluster)
     {
       std::ostringstream ss;
-      ss << "[pcl::extractEuclideanClusters] This cluster has " << seed_queue.size () <<
+      ss << "[pcl::extractEuclideanClusters] This cluster has " << found_points.size () <<
             " points, which is not between " << min_pts_per_cluster << " and " <<
             max_pts_per_cluster << " points, so it is not a final cluster\n";
       PCL_THROW_EXCEPTION(pcl::ComputeFailedException, ss.str ());
